6195ee3bd61c7ae0c0603e32a5dea872c60159c5
Lorraine Coelho <lorrainealisha75@gmail.com>
2020-10-12 17:47:36 +0200

    Test

--- run_autoupdate.sh
+++ run_autoupdate.sh
@@ -18,7 +18,7 @@ planemo --help
 git config --global user.email "lorrainealisha75@gmail.com"
 git config --global user.name "Lorraine Coelho"
 
-planemo clone --skip_fork --branch planemo-pr12-`date +%F-%H-%m` git@github.com:lorrainealisha75/galaxytools
+planemo clone --skip_fork --branch planemo-pr11-`date +%F-%H-%m` git@github.com:lorrainealisha75/galaxytools
 echo "Cloning successful"
 git remote add upstream git@github.com:simonbray/galaxytools
 cd galaxytools/
@@ -26,9 +26,7 @@ echo "Folder is:"
 pwd
 echo "Commit"
 git commit --allow-empty -m 'new test commit from planemo'
-echo "Git pull origin master"
-git pull origin master
 echo "Push"
-git push -u origin planemo-pr12-`date +%F-%H-%m`
+git push -u origin planemo-pr11-`date +%F-%H-%m`
 echo "Create pull request"
 planemo pull_request -m "planemo test pr 11"
