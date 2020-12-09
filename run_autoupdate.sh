1de142b2f46ffa3313a411912987a9301675f569
Lorraine Coelho <lorrainealisha75@gmail.com>
2020-12-09 11:36:56 +0100

    Minor changes

--- run_autoupdate.sh
+++ run_autoupdate.sh
@@ -13,12 +13,14 @@ planemo --help
 git config --global user.email "lorrainealisha75@gmail.com"
 git config --global user.name "Lorraine Coelho"
 
+echo "Folder before clonning is:"
+pwd
 #planemo clone --skip_fork --branch planemo-pr11-`date +%F-%H-%m` git@github.com:lorrainealisha75/galaxytools
 planemo clone --skip_fork --branch planemo-1 git@github.com:lorrainealisha75/galaxytools
 echo "Cloning successful"
 git remote add upstream git@github.com:lorrainealisha75/galaxytools
 cd galaxytools/
-echo "Folder is:"
+echo "Folder after cloning is:"
 pwd
 echo "Commit"
 git commit --allow-empty -m 'New test commit from planemo'
