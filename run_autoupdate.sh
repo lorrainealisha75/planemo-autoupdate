422c684ab96beecdc430adc5968b0e2bd037b1ef
Lorraine Coelho <lorrainealisha75@gmail.com>
2020-12-09 17:26:56 +0100

    Minor changes

--- run_autoupdate.sh
+++ run_autoupdate.sh
@@ -6,10 +6,10 @@ source .au/bin/activate
 
 
 # Install gh
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-sudo apt-add-repository https://cli.github.com/packages
-sudo apt update
-sudo apt install gh
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+#sudo apt-add-repository https://cli.github.com/packages
+#sudo apt update
+#sudo apt install gh
 
 
 # pip install conda
