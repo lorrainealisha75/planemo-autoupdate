9d33a27e401ed17be454f724742ef21bf11bd3ae
Lorraine Coelho <lorrainealisha75@gmail.com>
2020-12-09 15:10:44 +0100

    Minor changes

--- run_autoupdate.sh
+++ run_autoupdate.sh
@@ -20,15 +20,13 @@ echo "github.com:" >> $HOME/.planemo.yml
 echo "	- user: $GITHUB_USERNAME" >> $HOME/.planemo.yml
 echo "	  oauth_token: $GITHUB_TOKEN" >> $HOME/.planemo.yml
 
-cat $HOME/.planemo.yml
 
-cat $HOME/.planemo.yml
 echo "Folder before cloning is:"
 pwd
 #planemo clone --skip_fork --branch planemo-pr11-`date +%F-%H-%m` git@github.com:lorrainealisha75/galaxytools
 planemo clone --fork --branch planemo-1 git@github.com:simonbray/duck
 echo "Cloning successful"
-git remote add upstream git@github.com:lorrainealisha75/duck
+#git remote add upstream git@github.com:lorrainealisha75/duck
 cd duck/
 echo "Folder after cloning is:"
 pwd
@@ -36,7 +34,7 @@ echo "Commit"
 git commit --allow-empty -m 'New test commit from planemo'
 echo "Push"
 #git push -u origin planemo-pr11-`date +%F-%H-%m`
-git push -u upstream planemo-1
+git push -u origin planemo-1
 echo "Create pull request"
 git request-pull git@github.com:simonbray/duck master
 #planemo pull_request git@github.com:simonbray/duck master
