8e0b5ec11f3f9cdd2b3ed32b03b0d62cd52bb27f
Lorraine Coelho <lorrainealisha75@gmail.com>
2020-12-10 11:05:17 +0100

    Minor changes

--- run_autoupdate.sh
+++ run_autoupdate.sh
@@ -4,20 +4,6 @@ sudo pip3 install virtualenv
 virtualenv .au
 source .au/bin/activate
 
-
-# Install gh
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-#sudo apt-add-repository https://cli.github.com/packages
-#sudo apt update
-#sudo apt install gh
-
-
-# pip install conda
-#pip install https://github.com/simonbray/planemo/archive/autoupdate-sb.zip  # install from my fork for now
-#echo "installing planemo"
-#pip install -e planemo
-#planemo --help
-
 # git credentials
 git config --global user.email "lorrainealisha75@gmail.com"
 git config --global user.name "Lorraine Coelho"
