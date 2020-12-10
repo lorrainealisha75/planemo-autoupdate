71508f7c360ec82a34a3484ad97eea9759fb95b2
Lorraine Coelho <lorrainealisha75@gmail.com>
2020-12-10 17:56:01 +0100

    Minor changes

--- test.sh
+++ test.sh
@@ -9,5 +9,5 @@ git config user.email "lorrainealisha75@gmail.com"
 git config user.name "Lorraine Coelho"
 
 
-abc="https:/lorrainealisha75"
+abc="https://lorrainealisha75"
 echo $abc:$GITHUB_TOKEN"@github.com/abc.git"
