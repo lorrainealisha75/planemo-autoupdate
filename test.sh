fcbd7ee6a846a886765a1960f1e14ad908e93708
Lorraine Coelho <lorrainealisha75@gmail.com>
2020-12-10 17:03:38 +0100

    Minor changes

---
+++ test.sh
@@ -0,0 +1,11 @@
+echo "Starting autoupdate..."
+pip3 install wheel
+sudo pip3 install virtualenv
+virtualenv .au
+source .au/bin/activate
+
+# git credentials
+git config user.email "lorrainealisha75@gmail.com"
+git config user.name "Lorraine Coelho"
+
+echo "https://${{github.actor}}:$GITHUB_TOKEN@github.com/abc.git"
