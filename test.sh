55ef63666606e86c17a6633dd55d29bb3f4f4350
Lorraine Coelho <lorrainealisha75@gmail.com>
2020-12-11 15:27:01 +0100

    Minor changes

--- test.sh
+++ test.sh
@@ -1,12 +1,33 @@
-echo "Starting autoupdate..."
-pip3 install wheel
-sudo pip3 install virtualenv
-virtualenv .au
-source .au/bin/activate
+		echo "Starting autoupdate..."
+        pip3 install wheel
+        sudo pip3 install virtualenv
+        virtualenv .au
+        source .au/bin/activate
 
-# git credentials
-git config user.email "lorrainealisha75@gmail.com"
-git config user.name "Lorraine Coelho"
+        # git credentials
+        git config --global user.email "lorrainealisha75@gmail.com"
+        git config --global user.name "Lorraine Coelho"
 
+        echo ${{ secrets.PAT }} > token.txt
 
-echo "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/abc.git"
+        # Authenticate
+        echo "Authenticating..."
+        gh auth login --with-token < token.txt
+
+        # Fork and clone repo
+        echo "Begin forking and cloning..."
+        gh repo fork simonbray/SARS-CoV-2 --clone --remote
+
+        cd SARS-CoV-2/
+
+        echo "Fork and clone successful"
+
+        # Commit
+        echo "Committing..."
+        git commit --allow-empty -m 'New test commit from planemo'
+
+        echo "Pushing..."
+        git push https://$GITHUB_ACTOR:${{ secrets.PAT }}@github.com/$GITHUB_ACTOR/SARS-CoV-2.git master
+
+        echo "Creating a PR..."
+        gh pr create --title "ABC" --body "XYZ" --repo simonbray/SARS-CoV-2
